2b9f13b5f49f44cdc109d04df608ae27592739d6
Yanto Caluwé <yantocaluwe1@gmail.com>
2026-06-07 18:24:33 +0200

    Added Rotate To Zombie task

---
+++ Plugins/GameFeatures/CaluweYantoZombie/Source/CaluweYantoZombieRuntime/Private/Tasks/BTTask_LookAtZombieCaluweYanto.cpp
@@ -0,0 +1,58 @@
+﻿#include "Tasks/BTTask_LookAtZombieCaluweYanto.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "GameFramework/Pawn.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UBTTask_LookAtZombieCaluweYanto::UBTTask_LookAtZombieCaluweYanto()
+{
+    NodeName = "Rotate To Nearest Zombie"; 
+    bNotifyTick = true;
+}
+
+EBTNodeResult::Type UBTTask_LookAtZombieCaluweYanto::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    AAIController* Controller = OwnerComp.GetAIOwner();
+    UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+
+    if (!Controller || !BlackboardComp) return EBTNodeResult::Failed;
+
+    AActor* Zombie = Cast<AActor>(BlackboardComp->GetValueAsObject(NearestZombieKey.SelectedKeyName));
+    if (!Zombie) return EBTNodeResult::Failed;
+ 
+    return EBTNodeResult::InProgress;
+}
+
+void UBTTask_LookAtZombieCaluweYanto::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+    AAIController* Controller = OwnerComp.GetAIOwner();
+    UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+
+    if (!Controller || !BlackboardComp)
+    {
+        FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+        return;
+    }
+
+    APawn* Pawn = Controller->GetPawn();
+    AActor* Zombie = Cast<AActor>(BlackboardComp->GetValueAsObject(NearestZombieKey.SelectedKeyName));
+
+    if (!Pawn || !Zombie)
+    {
+        FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+        return;
+    }
+ 
+    FVector LookAtVector = Zombie->GetActorLocation() - Pawn->GetActorLocation(); 
+    LookAtVector.Z = 0.f; 
+    
+    FRotator TargetRotation = LookAtVector.ToOrientationRotator(); 
+    FRotator CurrentRotation = Pawn->GetActorRotation();
+    FRotator NewRotation = UKismetMathLibrary::RInterpTo(CurrentRotation, TargetRotation, DeltaSeconds, RotationSpeed); 
+    Pawn->SetActorRotation(NewRotation);
+ 
+    if (FMath::Abs(NewRotation.Yaw - TargetRotation.Yaw) < 2.f)
+    { 
+        FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+    }
+}
---
+++ Plugins/GameFeatures/CaluweYantoZombie/Source/CaluweYantoZombieRuntime/Public/Tasks/BTTask_LookAtZombieCaluweYanto.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include  "CoreMinimal.h"
+#include  "BehaviorTree/BTTaskNode.h"
+#include "BTTask_LookAtZombieCaluweYanto.generated.h"
+
+// Make unreal class. and prefix U = UObject (garbage collected and memory managed)
+UCLASS()
+class  CALUWEYANTOZOMBIERUNTIME_API UBTTask_LookAtZombieCaluweYanto : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_LookAtZombieCaluweYanto();
+	
+protected:
+	// override Task, inheritance
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& root, uint8* nodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+		
+	UPROPERTY(EditAnywhere, Category = "Blackboard");
+	FBlackboardKeySelector NearestZombieKey;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	float RotationSpeed{5.f};
+	
+};
